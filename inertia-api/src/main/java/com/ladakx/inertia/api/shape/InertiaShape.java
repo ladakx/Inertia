61410431140f0342336cf6b296d96a7a23bdc30e
Ladakx <vlad1256a@gmail.com>
2025-09-24 11:53:36 +0300

    clear poop code

--- inertia-api/src/main/java/com/ladakx/inertia/api/shape/InertiaShape.java
+++
@@ -1,8 +0,0 @@
-package com.ladakx.inertia.api.shape;
-
-/**
- * A marker interface for all physics shapes in Inertia.
- * Custom shapes will implement this interface to be used with the BodyBuilder.
- */
-public interface InertiaShape {
-}
