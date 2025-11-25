adb349f4a9d0066c48189ccae90004ea8abdea02
Ladakx <vlad1256a@gmail.com>
2025-11-25 23:24:19 +0200

    fix gameProfile

--- inertia-common/build.gradle.kts
+++ inertia-common/build.gradle.kts
@@ -3,16 +3,12 @@ plugins {
 }
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+
 tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
+    options.encoding= "UTF-8"
     options.release.set(16)
 }
 
 dependencies {
-    compileOnly(project(":inertia-api"))
-    compileOnly(project(":inertia-common"))
-    compileOnly(project(":inertia-nms-abstraction"))
-
     compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-    compileOnly("com.mojang:authlib:3.13.56")
 }
