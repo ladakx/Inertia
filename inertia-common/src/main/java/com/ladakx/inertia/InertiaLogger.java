ad114d333fc48449389b12f86da3617ebed27dc5
Ladakx <vlad1256a@gmail.com>
2025-12-07 18:48:35 +0200

    upgrade logger

--- inertia-common/src/main/java/com/ladakx/inertia/InertiaLogger.java
+++ inertia-common/src/main/java/com/ladakx/inertia/InertiaLogger.java
@@ -24,14 +24,34 @@ public class InertiaLogger {
         logger = plugin.getLogger();
     }
 
+    public static void info(Throwable throwable) {
+        ensureInit();
+        logger.log(Level.INFO, "An unexpected error occurred:", throwable);
+    }
+
     public static void info(String message) {
         ensureInit();
         logger.info(message);
     }
 
+    public static void info(String message, Throwable throwable) {
+        ensureInit();
+        logger.log(Level.INFO, message, throwable);
+    }
+
+    public static void warn(Throwable throwable) {
+        ensureInit();
+        logger.log(Level.WARNING, "An unexpected error occurred:", throwable);
+    }
+
     public static void warn(String message) {
         ensureInit();
         logger.warning(message);
+    }
+
+    public static void warn(String message, Throwable throwable) {
+        ensureInit();
+        logger.log(Level.WARNING, message, throwable);
     }
 
     public static void error(String message) {
@@ -41,7 +61,7 @@ public class InertiaLogger {
 
     public static void error(String message, Throwable throwable) {
         ensureInit();
-        logger.log(java.util.logging.Level.SEVERE, message, throwable);
+        logger.log(Level.SEVERE, message, throwable);
     }
 
     public static void error(Throwable throwable) {
