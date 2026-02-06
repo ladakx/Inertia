93a269f9568a1b38c3916eda802e7e7efb0f9bac
Ladakx <vlad1256a@gmail.com>
2026-02-06 22:14:36 +0200

    Scope NetworkEntityTracker tick to world players

--- inertia-core/src/main/java/com/ladakx/inertia/rendering/RenderFactory.java
+++ inertia-core/src/main/java/com/ladakx/inertia/rendering/RenderFactory.java
@@ -7,8 +7,8 @@ import org.bukkit.World;
 import org.bukkit.util.Vector;
 
 public interface RenderFactory {
-    VisualEntity create(World world, Location origin, RenderEntityDefinition definition);
+    NetworkVisual create(World world, Location origin, RenderEntityDefinition definition);
 
     // Новый метод для создания дебаг-линий
-    VisualEntity createDebugLine(World world, Vector start, Vector end, float thickness, Color color);
+    NetworkVisual createDebugLine(World world, Vector start, Vector end, float thickness, Color color);
 }
