92b519d0821a580ebe1396e422d40f963a0f4a42
Ladakx <vlad1256a@gmail.com>
2026-02-23 18:38:34 +0200

    Add versioned physics event namespace and publish lifecycle/tick events

--- inertia-api/src/main/java/com/ladakx/inertia/api/events/PhysicsBodyDestroyEvent.java
+++ inertia-api/src/main/java/com/ladakx/inertia/api/events/PhysicsBodyDestroyEvent.java
@@ -1,31 +1,11 @@
 package com.ladakx.inertia.api.events;
 
 import com.ladakx.inertia.api.body.PhysicsBody;
-import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
-public class PhysicsBodyDestroyEvent extends Event {
-    private static final HandlerList handlers = new HandlerList();
-    private final PhysicsBody body;
-
+@Deprecated(forRemoval = false)
+public class PhysicsBodyDestroyEvent extends com.ladakx.inertia.api.events.physics.PhysicsBodyPostDestroyEvent {
     public PhysicsBodyDestroyEvent(@NotNull PhysicsBody body) {
-        this.body = body;
-    }
-
-    @NotNull
-    public PhysicsBody getBody() {
-        return body;
-    }
-
-    @NotNull
-    @Override
-    public HandlerList getHandlers() {
-        return handlers;
-    }
-
-    @NotNull
-    public static HandlerList getHandlerList() {
-        return handlers;
+        super(body);
     }
 }
